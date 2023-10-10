adbb9a525e3e5a45dce6ee745f169eb83ad6a169
Christopher Kenny <ctk44@cornell.edu>
2023-10-10 14:12:43 -0400

    fix test

--- tests/testthat/test-feed_feed.R
+++ tests/testthat/test-feed_feed.R
@@ -1,6 +1,6 @@
 with_mock_dir("t/f/feedfeed", {
   test_that("`bs_get_feed()` works", {
     x <- bs_get_feed('at://did:plc:z72i7hdynmk6r22z27h6tvur/app.bsky.feed.generator/bsky-team', auth = auth)
-    expect_s3_class(x, 'tbl_df')
+    expect_true(is.list(x))
   })
 })
