4b5dd4b50c612fbf587b0410eea87942bb423748
Christopher Kenny <ctk44@cornell.edu>
2023-10-11 13:38:41 -0400

    gate auth

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,3 +1,7 @@
 library(httptest2)
+if (has_bluesky_pass() && has_bluesky_user()) {
+  auth <- bs_auth(user = get_bluesky_user(), pass = get_bluesky_pass())
+} else {
+  auth <- NULL
+}
 
-auth <- bs_auth(user = get_bluesky_user(), pass = get_bluesky_pass())
