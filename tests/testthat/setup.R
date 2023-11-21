d937c95cce5b7d4dbc06280003ef47f23df8b2bb
Christopher Kenny <ctk44@cornell.edu>
2023-11-21 12:42:18 -0500

    simple fix passes on winbuilder

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -2,6 +2,6 @@ library(httptest2)
 if (has_bluesky_pass() && has_bluesky_user()) {
   auth <- bs_auth(user = get_bluesky_user(), pass = get_bluesky_pass())
 } else {
-  auth <- NULL
+  auth <- list(accessJwt = '')
 }
 
