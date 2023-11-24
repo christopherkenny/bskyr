5bbc2facf5146ad51e5c0d72165f5888d9962ce3
Christopher Kenny <ctk44@cornell.edu>
2023-11-24 13:44:47 -0500

    cleaning [ci skip]

--- NEWS.md
+++ NEWS.md
@@ -5,6 +5,7 @@
   * Images can be specified with the `images` argument.
   * Alt text for images can be specified with the `images_alt` argument.
 * Adds support for direct blob uploads with `bs_upload_blob()`. This powers the ability to add media to posts.
+* Adds `bs_uri_to_url()` which formats a given `uri` as an HTTPS URL.
 
 # bskyr 0.0.5
 
--- R/url_uri.R
+++ R/url_uri.R
@@ -9,5 +9,7 @@
 #' bs_uri_to_url('at://did:plc:ic6zqvuw5ulmfpjiwnhsr2ns/app.bsky.feed.post/3k7qmjev5lr2s')
 bs_uri_to_url <- function(uri) {
   pieces <- httr2::url_parse(uri)
-  paste0('https://bsky.app/profile/', pieces$hostname, ':', pieces$port, '/post/', stringr::word(pieces$path, start = 3, sep = stringr::fixed('/')))
+  type <- stringr::word(pieces$path, start = 2, sep = stringr::fixed('/'))
+  paste0('https://bsky.app/profile/', pieces$hostname, ':', pieces$port, '/', 'post',
+         '/', stringr::word(pieces$path, start = 3, sep = stringr::fixed('/')))
 }
