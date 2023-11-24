7c0d5fc63fffa6f2a991565b38caea78eb3559ea
Christopher Kenny <ctk44@cornell.edu>
2023-11-24 18:01:07 -0500

    add test for handle resolve

---
+++ tests/testthat/test-identity_resolve_handle.R
@@ -0,0 +1,6 @@
+with_mock_dir("t/i/resolveh", {
+  test_that("`bs_resolve_handle()` works", {
+    x <- bs_resolve_handle('chriskenny.bsky.social', auth = auth)
+    expect_s3_class(x, 'tbl_df')
+  })
+})
