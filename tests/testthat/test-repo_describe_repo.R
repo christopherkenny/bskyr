2e287c87dcca490b7ce6f2bf9e899cd3b085eae6
Christopher Kenny <ctk44@cornell.edu>
2023-11-25 22:33:42 -0500

    fix test

--- tests/testthat/test-repo_describe_repo.R
+++ tests/testthat/test-repo_describe_repo.R
@@ -1,6 +1,13 @@
-# with_mock_dir("t/r/descRepo", {
-#   test_that("`bs_describe_repo()` works", {
-#     x <- bs_describe_repo(repo = 'chriskenny.bsky.social', auth = auth)
-#     expect_s3_class(x, 'tbl_df')
-#   })
-# })
+with_mock_dir("t/r/descRepo", {
+  test_that("`bs_describe_repo()` works", {
+    x <- bs_describe_repo(repo = 'chriskenny.bsky.social', auth = auth)
+    expect_s3_class(x, 'tbl_df')
+  })
+})
+
+with_mock_dir("t/r/descRepoL", {
+  test_that("`bs_describe_repo()` works", {
+    x <- bs_describe_repo(repo = 'chriskenny.bsky.social', auth = auth, clean = TRUE)
+    expect_s3_class(x, 'tbl_df')
+  })
+})
