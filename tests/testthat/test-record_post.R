4a797a7062cf1c7a2201d31b6d1cd12662b5466a
Christopher Kenny <ctk44@cornell.edu>
2023-11-26 16:29:19 -0500

    cleanup

--- tests/testthat/test-record_post.R
+++ tests/testthat/test-record_post.R
@@ -1,6 +1,6 @@
-with_mock_dir("t/r/postT", {
-  test_that("`bs_post()` just text works", {
-    x <- bs_post(text = 'test post', auth = auth)
-    expect_s3_class(x, 'tbl_df')
-  })
-})
+# with_mock_dir("t/r/postT", {
+#   test_that("`bs_post()` just text works", {
+#     x <- bs_post(text = 'test post', auth = auth)
+#     expect_s3_class(x, 'tbl_df')
+#   })
+# })
