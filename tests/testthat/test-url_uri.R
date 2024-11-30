039caa30287905a9eb1ce9f640c21ab5f11e6e94
Christopher Kenny <ctk44@cornell.edu>
2024-11-30 15:45:43 -0500

    Update test-url_uri.R

--- tests/testthat/test-url_uri.R
+++ tests/testthat/test-url_uri.R
@@ -8,7 +8,7 @@ test_that('uri to url works', {
 with_mock_dir('t/o/url2uri', {
   test_that('urls to uri works', {
     expect_equal(
-      bs_url_to_uri('https://bsky.app/profile/chriskenny.bsky.social/post/3lc5d6zspys2c'),
+      bs_url_to_uri('https://bsky.app/profile/chriskenny.bsky.social/post/3lc5d6zspys2c', auth = auth),
       'at://did:plc:wpe35pganb6d4pg4ekmfy6u5/app.bsky.feed.post/3lc5d6zspys2c'
     )
   })
