8ce75843a317d4a6b8abb2b50dc6430d839f0677
Christopher Kenny <ctk44@cornell.edu>
2024-12-01 18:35:57 -0500

    Update test-graph_relationships.R

--- tests/testthat/test-graph_relationships.R
+++ tests/testthat/test-graph_relationships.R
@@ -1,6 +1,6 @@
 with_mock_dir('t/g/relationships', {
   test_that('`bs_get_relationships()` works', {
-    x <- bs_get_relationships('did:plc:wpe35pganb6d4pg4ekmfy6u5', 'did:plc:5c2r73erhng4bszmxlfdtscf')
+    x <- bs_get_relationships('did:plc:wpe35pganb6d4pg4ekmfy6u5', 'did:plc:5c2r73erhng4bszmxlfdtscf', auth = auth)
     expect_s3_class(x, 'tbl_df')
   })
 })
