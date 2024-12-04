751be2a5d0944903c6b6700bd3c2645c5ac9745d
Christopher Kenny <ctk44@cornell.edu>
2024-12-03 21:02:21 -0500

    support blocking / unblocking

--- R/record_delete_wrappers.R
+++ R/record_delete_wrappers.R
@@ -169,3 +169,39 @@ bs_delete_repost <- function(rkey,
     auth = auth
   )
 }
+
+#' Delete a block
+#'
+#' @param rkey `r template_var_rkey()`
+#' @param user `r template_var_user()`
+#' @param pass `r template_var_pass()`
+#' @param auth `r template_var_auth()`
+#'
+#' @concept repo
+#'
+#' @return an `httr2` status code
+#' @export
+#'
+#' @section Lexicon references:
+#' [graph/list.json (2024-12-03)](https://github.com/bluesky-social/atproto/blob/main/lexicons/app/bsky/graph/follow.json)
+#' [repo/deleteRecord.json (2024-12-01)](https://github.com/bluesky-social/atproto/blob/main/lexicons/com/atproto/repo/deleteRecord.json)
+#'
+#' @section Function introduced:
+#' `v0.2.0` (2024-12-03)
+#'
+#' @examplesIf has_bluesky_pass() && has_bluesky_user()
+#' blck <- bs_block(subject = 'nytimes.com')
+#' bs_delete_block(bs_extract_record_key(blck$uri))
+bs_delete_block <- function(rkey,
+                             user = get_bluesky_user(), pass = get_bluesky_pass(),
+                             auth = bs_auth(user, pass)) {
+  bs_delete_record(
+    collection = 'app.bsky.graph.block',
+    rkey = rkey,
+    auth = auth
+  )
+}
+
+#' @rdname bs_delete_block
+#' @export
+bs_unblock <- bs_delete_block
