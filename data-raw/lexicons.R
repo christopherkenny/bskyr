854978b5a0b2ec62a8dcc6d94f38136c7fdf35fe
Christopher Kenny <ctk44@cornell.edu>
2024-12-04 18:28:34 -0500

    Update lexicons.R

--- data-raw/lexicons.R
+++ data-raw/lexicons.R
@@ -12,16 +12,33 @@ library(tidyverse)
 current_supported <- fs::dir_ls('R') |>
   lapply(function(x) {
     lns <- readLines(x)
-    lns |>
-      purrr::keep(stringr::str_detect(lns, '\\[.+/.+.json'))
+
+    lex_lns <- which(stringr::str_detect(lns, '\\[.+/.+.json'))
+    fn_lns <- which(stringr::str_detect(lns, ' <- function\\('))
+
+    keeps <- map_int(lex_lns, function(x) {
+      fn_lns[fn_lns > x][1]
+    })
+
+    lex <- lns[lex_lns]
+    fns <- lns[keeps]
+
+    if (length(lex_lns) > 0) {
+      tibble::tibble(
+        file = x,
+        line = lex,
+        fn = fns
+      )
+    } else {
+      NULL
+    }
   }) |>
-  tibble::enframe(name = 'file', value = 'line') |>
-  tidyr::unnest_longer(line) |>
+  list_rbind() |>
   dplyr::mutate(
     lexicon = stringr::str_extract(line, '\\[(.+/.+).json '),
     lexicon = stringr::str_sub(lexicon, 2, -2),
-    full = stringr::str_extract(line, 'lexicons/.+/.+/.+/.+'),
-    full = stringr::str_sub(full, 10, -2)
+    ref = stringr::str_extract(line, 'lexicons/.+/.+/.+/.+'),
+    ref = stringr::str_sub(ref, 10, -2)
   )
 
 # what is the full set of lexicons
@@ -33,9 +50,11 @@ non_def_lexicons <- all_lexicons |>
   discard_at(at = stringr::str_detect(all_lexicon_files, 'defs'))
 
 types <- tibble::tibble(
-  file = names(non_def_lexicons),
-  full = str_remove(file, '../atproto/lexicons/'),
+  local_file = names(non_def_lexicons),
+  ref = str_remove(local_file, '../atproto/lexicons/'),
   type = map_chr(non_def_lexicons, function(x) x$defs$main$type)
 )
 
-t
+types |>
+  left_join(current_supported, by = 'ref') |>
+  filter(type == 'record', is.na(fn))
