b03578de1256e5599ccdc794a97932985740a77d
Christopher Kenny <ctk44@cornell.edu>
2025-02-08 16:32:40 -0500

    small edits for cran fix

--- tests/testthat/test-graph_relationships.R
+++ tests/testthat/test-graph_relationships.R
@@ -1,4 +1,4 @@
-with_mock_dir('t/g/relationships', {
+with_mock_dir('t/g/rel', {
   test_that('`bs_get_relationships()` works', {
     x <- bs_get_relationships('did:plc:wpe35pganb6d4pg4ekmfy6u5', 'did:plc:5c2r73erhng4bszmxlfdtscf', auth = auth)
     expect_s3_class(x, 'tbl_df')
