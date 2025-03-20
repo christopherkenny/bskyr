b27a10987caee1d9d4189efd09eca401b41bc958
Christopher Kenny <ctk44@cornell.edu>
2025-03-20 17:54:06 -0400

    handle #26 ~ adds cursor and repeated requests

--- R/graph_list.R
+++ R/graph_list.R
@@ -1,6 +1,7 @@
 #' Get a view of a list
 #'
 #' @param list `r template_var_list()`
+#' @param cursor `r template_var_cursor()`
 #' @param limit `r template_var_limit(100)`
 #' @param user `r template_var_user()`
 #' @param pass `r template_var_pass()`
@@ -13,14 +14,15 @@
 #' @export
 #'
 #' @section Lexicon references:
-#' [graph/getList.json (2024-11-25)](https://github.com/bluesky-social/atproto/blob/main/lexicons/app/bsky/graph/getList.json)
+#' [graph/getList.json (2025-03-20)](https://github.com/bluesky-social/atproto/blob/main/lexicons/app/bsky/graph/getList.json)
 #'
 #' @section Function introduced:
 #' `v0.2.0` (2024-11-25)
 #'
 #' @examplesIf has_bluesky_pass() && has_bluesky_user()
 #' bs_get_list('at://did:plc:ragtjsm2j2vknwkz3zp4oxrd/app.bsky.graph.list/3kmokjyuflk2g')
-bs_get_list <- function(list, limit = NULL,
+#' bs_get_list('at://did:plc:hgyzg2hn6zxpqokmp5c2xrdo/app.bsky.graph.list/3laygnmmcfc2x')
+bs_get_list <- function(list, cursor = NULL, limit = NULL,
                         user = get_bluesky_user(), pass = get_bluesky_pass(),
                         auth = bs_auth(user, pass), clean = TRUE) {
 
@@ -37,7 +39,9 @@ bs_get_list <- function(list, limit = NULL,
     }
     limit <- as.integer(limit)
     limit <- max(limit, 1L)
-    limit <- min(limit, 100L)
+    req_seq <- diff(unique(c(seq(0, limit, 100), limit)))
+  } else {
+    req_seq <- list(NULL)
   }
 
   req <- httr2::request('https://bsky.social/xrpc/app.bsky.graph.getList') |>
@@ -46,24 +50,30 @@ bs_get_list <- function(list, limit = NULL,
     httr2::req_url_query(
       limit = limit
     )
-  resp <- req |>
-    httr2::req_perform() |>
-    httr2::resp_body_json()
+
+  resp <- repeat_request(req, req_seq, cursor, txt = 'Fetching list views')
 
    if (!clean) {
      return(resp)
    }
 
-  dplyr::bind_cols(
-    resp |>
-      purrr::pluck('items') |>
-      list_hoist() |>
-      clean_names(),
-    resp |>
-      purrr::pluck('list') |>
-      widen() |>
-      clean_names() |>
-      dplyr::rename_with(.fn = function(x) paste0('list_', x))
-  ) |>
+  lapply(resp, function(f) {
+    dplyr::bind_cols(
+      f |>
+        purrr::pluck('items') |>
+        list_hoist() |>
+        clean_names(),
+      f |>
+        purrr::pluck('list') |>
+        widen() |>
+        clean_names() |>
+        dplyr::rename_with(.fn = function(x) paste0('list_', x))
+    )
+  }) |>
+    purrr::list_rbind() |>
+    clean_names() |>
+    add_req_url(req) |>
+    add_cursor(resp) |>
     clean_names()
+
 }
