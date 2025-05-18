86e7bc5a6370c2be34b8cb6ff0708ad73189574c
Christopher Kenny <ctk44@cornell.edu>
2025-05-17 21:10:06 -0400

    fixes

--- R/roxygen.R
+++ R/roxygen.R
@@ -129,3 +129,7 @@ template_var_subject <- function() { # nocov start
 template_var_created_at <- function() { # nocov start
   'Character, length 1 of the form "%Y-%m-%dT%H:%M:%OS6Z". Time to assign to a record. Default is `bs_created_at()`.'
 } # nocov end
+
+template_var_convo_id <- function() { # nocov start
+  'Character, length 1. ID of the conversation to get.'
+} # nocov end
