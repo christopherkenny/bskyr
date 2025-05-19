0dac185b624f0ac8c8586f5d26a50d56c1a9f59d
Christopher Kenny <ctk44@cornell.edu>
2025-05-19 16:22:36 -0400

    expand tests

--- tests/testthat/test-feed_likes.R
+++ tests/testthat/test-feed_likes.R
@@ -1,5 +1,5 @@
 test_that('`bs_get_likes()` works', {
   vcr::local_cassette('f_getlikes')
-  x <- bs_get_likes('chriskenny.bsky.social', auth = auth)
+  x <- bs_get_likes('bskyr.bsky.social', auth = auth)
   expect_s3_class(x, 'tbl_df')
 })
--- tests/testthat/test-record_like.R
+++ tests/testthat/test-record_like.R
@@ -1,11 +1,11 @@
-#   test_that("`bs_like()` works", {
-#     vcr::local_cassette('r_like')
-#     x <- bs_like(post = rcd, auth = auth)
-#     expect_s3_class(x, 'tbl_df')
-#   })
-#
-#   test_that("`bs_like()` works", {
-#     vcr::local_cassette('r_likeL')
-#     x <- bs_like(post = rcd, auth = auth, clean = FALSE)
-#     expect_equal(class(x), 'list')
-#   })
+test_that("`bs_like()` works", {
+  vcr::local_cassette('r_like')
+  x <- bs_like(post = rcd, auth = auth)
+  expect_s3_class(x, 'tbl_df')
+})
+
+test_that("`bs_like()` works", {
+  vcr::local_cassette('r_likeL')
+  x <- bs_like(post = rcd, auth = auth, clean = FALSE)
+  expect_equal(class(x), 'list')
+})
