6702b5672533ef9b98ad175848472aa12fe7cd32
Christopher Kenny <ctk44@cornell.edu>
2025-05-19 01:22:58 -0400

    Update test-embed_external.R

--- tests/testthat/test-embed_external.R
+++ tests/testthat/test-embed_external.R
@@ -1,6 +1,7 @@
 test_that('`bs_new_embed_external()` works', {
   embed <- bs_new_embed_external(
-    uri = 'https://christophertkenny.com/bskyr/'
+    uri = 'https://christophertkenny.com/bskyr/',
+    auth = auth
   )
   expect_vector(embed, vector(mode = 'list'))
 })
