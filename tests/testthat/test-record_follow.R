5027b9a5791bfd2ace70b8f3805370ba89f674ea
Christopher Kenny <ctk44@cornell.edu>
2025-07-25 16:56:24 -0400

    test matching by uri/method

--- tests/testthat/test-record_follow.R
+++ tests/testthat/test-record_follow.R
@@ -1,5 +1,5 @@
 test_that('`bs_follow()` works', {
-  vcr::local_cassette('r_post_follow')
+  vcr::local_cassette('r_post_follow', match_requests_on = c('uri', 'method'))
   x <- bs_follow(subject = 'chriskenny.bsky.social', auth = auth)
   expect_s3_class(x, 'tbl_df')
 })
