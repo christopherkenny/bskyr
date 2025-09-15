2dbfbee939a650f61e270aa4d8799712c037e64f
Christopher Kenny <ctk44@cornell.edu>
2025-09-15 11:43:04 -0400

    test tidying

--- tests/testthat/test-feed_likes.R
+++ tests/testthat/test-feed_likes.R
@@ -3,3 +3,9 @@ test_that('`bs_get_likes()` works', {
   x <- bs_get_likes('bskyr.bsky.social', auth = auth)
   expect_s3_class(x, 'tbl_df')
 })
+
+test_that('`bs_get_likes()` fails on non-user', {
+  expect_error({
+    bs_get_likes('chriskenny.bsky.social', auth = auth)
+  })
+})
--- tests/testthat/test-feed_quotes.R
+++ tests/testthat/test-feed_quotes.R
@@ -1,4 +1,4 @@
-test_that('`bs_get_likes()` works', {
+test_that('`bs_get_quotes()` works', {
   vcr::local_cassette('f_getquotes')
   x <- bs_get_quotes('at://did:plc:5c2r73erhng4bszmxlfdtscf/app.bsky.feed.post/3lc5c5qv72r2w', auth = auth)
   expect_s3_class(x, 'tbl_df')
